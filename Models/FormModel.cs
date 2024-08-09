c00435be85ca802830ed41291be53a4607e56913
DeepankarDRS <151986138+DeepankarDRS@users.noreply.github.com>
2024-08-09 13:01:08 +0530

    Create FormModel.cs

---
+++ Models/FormModel.cs
@@ -0,0 +1,7 @@
+public class FormModel
+{
+    public string UserEmail { get; set; }
+    public string Detail1 { get; set; }
+    public string Detail2 { get; set; }
+    // Add other form fields as needed
+}
